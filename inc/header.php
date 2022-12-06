e6e172ddd81fabefbdf61ea5d8921074f6a91554
Carl Andreas Berge Bjørnseth <carlandbb@gmail.com>
2022-12-06 13:25:30 +0100

    s
    s

--- inc/header.php
+++ inc/header.php
@@ -1,8 +1,14 @@
 <?php 
-    $header = "<h1>Carl Andreas Berge Bjørnseth <span id='headersoan'>Port</span></h1>"; 
-    $globalnav = "<nav class=''>
-                    </nav>"
+    $header = "<h1>Carl Andreas Berge Bjørnseth <span id='headersoan'>Portefølje</span></h1>"; 
+    $globalnav = "<nav class='global_nav'>
+                    <ul>
+                        <li><a href='index.php'>Bio</a></li>
+                        <li><a href='web.php' >Web Design</a></li>
+                        <li><a href='ux.php' >UX</a></li>
+                        <li><a href='figma.php' >Figma</a></li>
+                        <li><a href='edu.php' >Edutainment</a></li>
+                    </ul>
+                </nav>";
     
-    $header .=  
-
+    $header .= $globalnav;
     ?>
