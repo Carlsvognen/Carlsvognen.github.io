aa2e182d75d71a846eacde0a960868e617f4f7cf
Carl Andreas Berge Bjørnseth <carlandbb@gmail.com>
2022-12-06 12:11:59 +0100

    s
    s

---
+++ inc/header.php
@@ -0,0 +1,6 @@
+<h1>
+    Carl Andreas Berge Bjørnseth <span id=''></span>
+</h1>
+<nav>
+
+</nav>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <header>
+            <?php include 'inc/header.php'; ?>
+        </header>
+        <main>
+            <h1>Hello World</h1>
+            <p>I'm hosted with GitHub Pages.</p>
+        </main>
+        
+    </body>
+</html>
