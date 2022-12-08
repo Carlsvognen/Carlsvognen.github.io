d009db847ceb074858a31fbd803fe09891436f4b
Carl Andreas Berge Bjørnseth <carlandbb@gmail.com>
2022-12-08 11:26:20 +0100

    s
    s

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,18 +1,18 @@
 //scrip borrowed by © ljc-dev https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
 
-const menu = document.querySelector(".menu");
+const navMenu = document.querySelector(".navMenu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelectorAll(".hamburger");
 const openOpt = document.querySelectorAll(".openOpt");
 const closeOpt = document.querySelectorAll(".closeOpt");
 
 function toggleMenu() {
-    if (menu.classList.contains("menuVisability")){
-        menu.classList.remove("menuVisability");
+    if (navMenu.classList.contains("menuVisability")){
+        navMenu.classList.remove("menuVisability");
         closeOpt.style.display = "none";
         openOpt.style.display = "block";
     } else {
-        menu.classList.add("menuVisability");
+        navMenu.classList.add("menuVisability");
         closeIcon.style.display = "block";
         openOpt.style.display = "none";
     }
